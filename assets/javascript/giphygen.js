4d59712819585ebb85fabab50083a5a4befefc19
ohmez <kkid_92@hotmail.com>
2018-09-27 15:07:55 -0600

    pulls 3 and populates 3, need to make 3 of those

--- assets/javascript/giphygen.js
+++ assets/javascript/giphygen.js
@@ -7,6 +7,16 @@ $(document).on("click", "#add", function(event) {
     $("input#input").val('');
     
 });
+$(document).on("click", ".gif", function(event) {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 function getGiph() {
     var aKey = '&api_key=92vKLfVYVZ51WHhYfCVLwPZbuJurXFc1&limit=10';
@@ -18,23 +28,33 @@ function getGiph() {
         for (i = 0; i < 11; i ++) {
             objs[i] = giph.data[i];
         }
-        populate();
+        create();
+        create();
         console.log(objs[0]);
         console.log(objs[0].rating);
         console.log(objs[0].images);
-        console.log(objs[0].images.downsized.url);
+        console.log(objs[0].images.original.url);
+        console.log(objs[0].images.original_still.url);
     });
     
 };
 
-function populate() {
+function create() {
     var newDivR = $("<div>").attr("class","row");
-    var a = $("<img>").attr("src",objs[0].images.downsized_still.url);
-    var b = $("<h5>").html("rating: " + objs[0].rating);
-    var c = $("<div>").html(b);
-    c.append(a);
-    newDivR.html(c);
     $("#giphsA").empty();
-    $("#giphsA").html(newDivR);
+    for (i = 0; i < 3; i ++ ) {
+        var a = $("<img>").attr({
+            class: 'gif',
+            src: objs[i].images.original_still.url, 
+            "data-still":objs[i].images.original_still.url,
+            "data-animate":objs[i].images.original.url,
+            "data-state": "still"
+        });
+        var b = $("<h5>").html("rating: " + objs[i].rating);
+        var c = $("<div>").html(b);
+        c.append(a);
+        newDivR.append(c);
+    }   
+    $("#giphsA").append(newDivR);
 
 }
