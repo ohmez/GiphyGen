e272a9c9d90e1ec9bc090215ebe5502e160c8321
ohmez <kkid_92@hotmail.com>
2018-09-27 17:03:08 -0600

    all 10 images work

--- assets/javascript/giphygen.js
+++ assets/javascript/giphygen.js
@@ -1,3 +1,4 @@
+var characters = ['naruto', 'sasuke', 'sakura', 'hinata', 'choji', 'kakashi', ]
 var add; 
 var objs = [];
 $(document).on("click", "#add", function(event) {
@@ -19,8 +20,8 @@ $(document).on("click", ".gif", function(event) {
   });
 
 function getGiph() {
-    var aKey = '&api_key=92vKLfVYVZ51WHhYfCVLwPZbuJurXFc1&limit=10';
-    var url = 'http://api.giphy.com/v1/gifs/search?q=';
+    var aKey = '&api_key=92vKLfVYVZ51WHhYfCVLwPZbuJurXFc1&limit=11';
+    var url = 'http://api.giphy.com/v1/gifs/search?q=naruto+';
     $.ajax({
         url: url + add + aKey,
         method: "GET"
@@ -28,13 +29,11 @@ function getGiph() {
         for (i = 0; i < 11; i ++) {
             objs[i] = giph.data[i];
         }
-        create();
-        create();
-        console.log(objs[0]);
-        console.log(objs[0].rating);
-        console.log(objs[0].images);
-        console.log(objs[0].images.original.url);
-        console.log(objs[0].images.original_still.url);
+        setTimeout(create(),200);
+        setTimeout(create2(),200);
+        setTimeout(create3(),200);
+        console.log(objs);
+        
     });
     
 };
@@ -43,6 +42,42 @@ function create() {
     var newDivR = $("<div>").attr("class","row");
     $("#giphsA").empty();
     for (i = 0; i < 3; i ++ ) {
+        var a = $("<img>").attr({
+            class: 'gif',
+            src: objs[i].images.original_still.url, 
+            "data-still":objs[i].images.original_still.url,
+            "data-animate":objs[i].images.original.url,
+            "data-state": "still"
+        });
+        var b = $("<h5>").html("rating: " + objs[i].rating);
+        var c = $("<div>").html(b);
+        c.append(a);
+        newDivR.append(c);
+    }   
+    $("#giphsA").append(newDivR);
+
+};
+function create2() {
+    var newDivR = $("<div>").attr("class","row");
+    for (i = 3; i < 7; i ++ ) {
+        var a = $("<img>").attr({
+            class: 'gif',
+            src: objs[i].images.original_still.url, 
+            "data-still":objs[i].images.original_still.url,
+            "data-animate":objs[i].images.original.url,
+            "data-state": "still"
+        });
+        var b = $("<h5>").html("rating: " + objs[i].rating);
+        var c = $("<div>").html(b);
+        c.append(a);
+        newDivR.append(c);
+    }   
+    $("#giphsA").append(newDivR);
+
+};
+function create3() {
+    var newDivR = $("<div>").attr("class","row");
+    for (i = 7; i < 10; i ++ ) {
         var a = $("<img>").attr({
             class: 'gif',
             src: objs[i].images.original_still.url, 
